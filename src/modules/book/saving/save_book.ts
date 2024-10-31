97a4db2264e63e14c1237437f2c613c716ca4298
MRB <greymon647@gmail.com>
2024-10-31 15:34:53 -0700

    mrb: fix sql

--- src/modules/book/saving/save_book.ts
+++ src/modules/book/saving/save_book.ts
@@ -4,7 +4,7 @@ export function saveBook(title: string, description: string): Promise<boolean> {
     return new Promise((resolve, reject) => {
         console.log("Saving a book, t: " + title + " desc: " + description);
 
-        var sql = "INSERT INTO books (title, description) VALUES (?, ?)";
+        var sql = "INSERT INTO books (title, descrp) VALUES (?, ?)";
 
         mysqlConnection.query(sql, [title, description], function (err, result) {
             if (err) {
