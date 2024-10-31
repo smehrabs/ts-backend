dea435daeccf13e19745a1d2a7b1210ca11cf2b0
MRB <greymon647@gmail.com>
2024-10-31 15:30:07 -0700

    mrb: add error handler for saver

--- src/modules/book/saving/save_book.ts
+++ src/modules/book/saving/save_book.ts
@@ -4,11 +4,17 @@ export function saveBook(title: string, description: string): Promise<boolean> {
     return new Promise((resolve, reject) => {
         console.log("Saving a book, t: " + title + " desc: " + description);
 
-        var sql = "INSERT INTO books (title, descrp) VALUES (?, ?)";
+        var sql = "INSERT INTO books (title, description) VALUES (?, ?)";
 
         mysqlConnection.query(sql, [title, description], function (err, result) {
             if (err) {
-                reject(err);
+                if (err.code === 'ER_DUP_ENTRY') {
+                    console.error("Duplicate entry error: A book with this title already exists.");
+                    resolve(false);
+                } else {
+                    console.error("Error inserting record: ", err);
+                    reject(err);
+                }
             } else {
                 console.log("[saving] 1 record inserted");
                 resolve(true);
