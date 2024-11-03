a565f593e95936a0589b72d08b60c9a27a8f1cff
MRB <greymon647@gmail.com>
2024-11-03 12:31:08 -0800

    mrb: add helmet

--- index.ts
+++ index.ts
@@ -6,9 +6,31 @@ import { config } from "./src/config/get";
 import booksRouter from "./src/routes";
 
 import "./src/init"; // init
+import helmet from "helmet";
 
 // app (express)
 const app = express()
+
+app.use(helmet({
+  contentSecurityPolicy: {
+      directives: {
+          defaultSrc: ["'self'"],
+          scriptSrc: ["'self'", "https://forvest.io"],
+          objectSrc: ["'none'"],
+          upgradeInsecureRequests: [],
+      },
+  },
+  hidePoweredBy: true,
+  frameguard: { action: 'deny' },
+  xssFilter: true,
+  noSniff: true,
+  hsts: {
+      maxAge: 31536000, // 1 year
+      includeSubDomains: true,
+      preload: true,
+  },
+  referrerPolicy: { policy: 'no-referrer' },
+}));
 
 app.use(express.json());
 
