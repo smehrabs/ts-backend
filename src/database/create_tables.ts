a27ce707fc68c7eda1d16f4f43f4095de81877be
MRB <greymon647@gmail.com>
2024-11-03 15:01:52 +0330

    mrb: add outDir

--- src/database/create_tables.ts
+++ src/database/create_tables.ts
@@ -1,5 +1,5 @@
 import { mysqlConnection } from "@config/conn";
-import { CREATE_TABLES } from "../../sql/book";
+import { CREATE_TABLES } from "@../sql/book";
 
 export default function () {
     mysqlConnection.connect(function (err: any) {
