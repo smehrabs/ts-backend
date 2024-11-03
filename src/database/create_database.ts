cc60b6c1e2f54b804ccd5686a87dd789e7686797
MRB <greymon647@gmail.com>
2024-11-03 10:17:33 -0800

    mrb: add init query (Mysql)

--- src/database/create_database.ts
+++ src/database/create_database.ts
@@ -1,6 +1,7 @@
 
-import { CREATE_DATABASE } from "../../sql/init";
+import { CREATE_DATABASE, CR_ALERT, CR_FLUSH, CR_USE } from "../../sql/init";
 import { con } from "../config/conn";
+import mysql from "mysql2";
 
 export default function (): Promise<boolean> {
   return new Promise((resolve, reject) => {
@@ -8,24 +9,38 @@ export default function (): Promise<boolean> {
     con.connect(function (err: any) {
       if (err) reject(err);
       console.log("[database1] connected!");
-      con.query(CREATE_DATABASE, function (err: any, result: any) {
-        if (err) {
-          reject(err);
+      // con.query(CREATE_DATABASE, function (err: any, result: any) {
+      //   if (err) {
+      //     reject(err);
+      //   } else {
+      //     console.log("[database1] checked!");
+      //     con.end(); // check and exit
+      //     console.log("[database1] closed!");
+      //     resolve(true);
+      //   }
+      // });
+
+      try {
+        const result = con.query(CREATE_DATABASE) as unknown as [mysql.ResultSetHeader];
+  
+        if (result[0].affectedRows > 0) {
+          console.log('Database created successfully.');
+          con.query(CR_USE);
+          con.query(CR_ALERT);
+          con.query(CR_FLUSH);
+          console.log('User altered and privileges flushed successfully.');
         } else {
-          console.log("[database1] checked!");
-          con.end(); // check and exit
-          console.log("[database1] closed!");
-          resolve(true);
+          console.log('Database already exists. Skipping user alteration.');
         }
-      });
+      } catch (error) {
+        console.error('Error executing queries:', error);
+        resolve(false);
+      } finally {
+        con.end();
+        resolve(true);
+      }
+
     });
 
   });
 }
-
-/**
- * ALTER USER 'mrb'@'localhost' IDENTIFIED WITH mysql_native_password BY 'PASSWORD';
- * flush privileges;
- * 
- * var sql = "CREATE TABLE IF NOT EXISTS books (id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255), descrp VARCHAR(255))";
- */
