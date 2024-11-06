12d6f336a6c975ae94151a48cef825befc0116c3
MRB <greymon647@gmail.com>
2024-11-06 13:32:09 +0330

    mrb: Reformat to Prettier & cu

--- src/databases/c-use.ts
+++ src/databases/c-use.ts
@@ -1,12 +1,11 @@
 // check and use
 
-import { config } from '../config/env_get.js';
-import { DatabasesUseType } from '../ts/types.js';
-import df_config from '../config/defaults.js'
+import { config } from "../config/env_get.js";
+import { DatabasesUseType } from "../ts/types.js";
+import df_config from "../config/defaults.js";
 
 export default function (): DatabasesUseType {
-    if (config.database_use == "mongo" || config.database_use == "mysql") {
-        return config.database_use
-    } else
-        return df_config.env.database_use
+  if (config.database_use == "mongo" || config.database_use == "mysql") {
+    return config.database_use;
+  } else return df_config.env.database_use;
 }
