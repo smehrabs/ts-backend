37bc4daa0d69905d77bd4bcc9a2c3d8ff50f37ce
MRB <greymon647@gmail.com>
2024-11-06 12:09:21 +0330

    mrb: [mysql] fix sqls

--- src/databases/mysql/sql/book.ts
+++ src/databases/mysql/sql/book.ts
@@ -8,9 +8,9 @@ const CREATE_TABLES = `CREATE TABLE IF NOT EXISTS ` + config.tc_book_name +` (
     );
 `;
 
-const INSERT_BOOKS = "INSERT INTO books (title, descrp) VALUES (?, ?)";
-const SELECT_BOOKS = "SELECT * FROM books WHERE title = ?";
-const DROP_BOOKS_TABLE = "DROP TABLE IF EXISTS books;";
+const INSERT_BOOKS = "INSERT INTO " + config.tc_book_name + " (title, descrp) VALUES (?, ?)";
+const SELECT_BOOKS = "SELECT * FROM " + config.tc_book_name + " WHERE title = ?";
+const DROP_BOOKS_TABLE = "DROP TABLE IF EXISTS " + config.tc_book_name + ";";
 
 export {
     CREATE_TABLES, INSERT_BOOKS, SELECT_BOOKS, DROP_BOOKS_TABLE
