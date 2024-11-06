b579b1e2c2df83bd100667a14649f54d7767c207
MRB <greymon647@gmail.com>
2024-11-06 15:38:00 +0330

    mrb: fix/add getItemByTitle to find

--- src/databases/mongo/modules/find.ts
+++ src/databases/mongo/modules/find.ts
@@ -1,19 +1,23 @@
-import { mongo_ns } from "#ts/interfaces";
+import { mongo_ns } from "#ts/interfaces.js";
 import mongoose from "mongoose";
 
 export class ItemFind {
-  private itemModel;
+  private itemModel: mongoose.Model<mongo_ns.IItem>;
 
   constructor(itemModel: mongoose.Model<mongo_ns.IItem>) {
     this.itemModel = itemModel;
   }
 
-  public async getAllItems() {
+  public async getItemByTitle(title: string) {
     try {
-      const items = await this.itemModel.find();
-      console.log("All items:", items);
+      const item = await this.itemModel.findOne({ title });
+      if (item) {
+        console.log("Item found:", item);
+      } else {
+        console.log(`No item found with the title "${title}".`);
+      }
     } catch (error) {
-      console.error("Error fetching items:", error);
+      console.error("Error fetching item:", error);
     }
   }
 }
