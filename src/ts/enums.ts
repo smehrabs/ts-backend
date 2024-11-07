c61f2f85249b172ebed51cf01a4ac8a4def12b76
MRB <greymon647@gmail.com>
2024-11-07 15:21:25 +0330

    mrb: doc for enums

--- src/ts/enums.ts
+++ src/ts/enums.ts
@@ -1,8 +1,18 @@
+
+
+/**
+ * We have to make all the database modules that are separate similar
+ * And make the capabilities of each similar module in the databases similar to each other
+ */
+
+
 export enum MongoModuleNames {
   Function1 = "Function 1",
   Function2 = "Function 2",
   Function3 = "f3"
 }
+
+
 export enum MysqlModuleNames {
   Function1 = "Function 1",
   Function2 = "Function 2",
