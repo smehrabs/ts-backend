7947bab79ae5a3129dc62d204d9a86e0a6b5d78e
MRB <greymon647@gmail.com>
2024-11-07 14:57:14 +0330

    mrb: called -> option

--- src/ts/types.ts
+++ src/ts/types.ts
@@ -5,10 +5,11 @@ export type DatabasesUseType = "mongo" | "mysql";
 export type NamedModules = {
     name: DatabaseModuleNames;
     func: (...args: any[]) => Promise<any> | any | void;
+    called?: boolean;
 };
 
 export type DatabasesType = {
     name: DatabasesUseType;
     modules: NamedModules[];
-    called: boolean; // check one time caller for next version on Redis cache
+    called?: boolean; // check one time caller for next version on Redis cache
 };
