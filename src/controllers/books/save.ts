85005612f4087d3dd1613ed742256d3755051c21
MRB <greymon647@gmail.com>
2024-11-10 13:44:44 +0330

    mrb: fix controllers

--- src/controllers/books/save.ts
+++ src/controllers/books/save.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import Joi from "joi";
-import { saveBook } from "#databases/mysql/modules/save";
+import { call } from "#modules/c-call";
 const bookSchema = Joi.object({
   title: Joi.string().required(),
   description: Joi.string().required(),
@@ -11,13 +11,19 @@ export const saveBookController = async (req: Request, res: Response) => {
   if (error) {
     res.status(400).json({ error: error.details[0].message });
   } else {
-    const { title, description } = req.body;
+    const { title, description } = req.body; // TODO() Check this params with JOI
+    // try {
+    //   const result = await saveBook(title, description);
+    //   res.send({ success: result });
+    // } catch (error) {
+    //   console.error("Error on saving: ", error);
+    //   res.status(500).send("Error on saving!");
+    // }
     try {
-      const result = await saveBook(title, description);
+      const result = await call("save", title, description);
       res.send({ success: result });
     } catch (error) {
-      console.error("Error on saving: ", error);
-      res.status(500).send("Error on saving!");
+      res.status(500).send("err");
     }
   }
 };
