1657c4c3fe2fead2692eb390c84d2af406fc988a
MRB <greymon647@gmail.com>
2024-11-10 14:09:40 +0330

    mrb: add versions to routes

---
+++ src/routes/latest/index.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import {
+  getBookController,
+  saveBookController,
+  dropBookController,
+} from "#controllers/index";
+import checkAdmin from "#middleware/checkAdmin";
+import { loginController } from "#controllers/admin/login";
+
+const router = Router();
+
+router.post("/login", loginController); // endpoint
+router.get("/get", checkAdmin, getBookController);
+router.post("/save", checkAdmin, saveBookController);
+router.post("/drop", checkAdmin, dropBookController);
+
+export default router;
