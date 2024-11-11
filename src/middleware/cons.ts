19f01cea7f90ccbdbf3bcd160eb630f40c04e9dd
MRB <greymon647@gmail.com>
2024-11-11 12:10:31 +0330

    mrb: fix checkIP for IPv4 only

--- src/middleware/cons.ts
+++ src/middleware/cons.ts
@@ -3,24 +3,29 @@ import { config } from "#config/env_get";
 import { Request, Response, NextFunction } from "express";
 
 const checkIP = (req: Request, res: Response, next: NextFunction): void => {
-  const ip = req.headers["x-forwarded-for"]
-    ? Array.isArray(req.headers["x-forwarded-for"])
-      ? req.headers["x-forwarded-for"][0]
-      : req.headers["x-forwarded-for"]
-    : req.socket.remoteAddress;
-
-  if (!ip) {
-    res.status(403).send(blockIpMsg);
-    return;
-  }
-
-  const clientIP = ip.includes(":") ? ip.split(":").pop() : ip;
-  console.log(`ip: ${clientIP}  ${ip}`)
+  const clientIP = req.ip;
 
   if (clientIP && config.ALLOWED_IPS.includes(clientIP)) {
     next();
   } else {
-    res.status(403).send(blockIpMsg);
+    const ip = req.headers["x-forwarded-for"]
+      ? Array.isArray(req.headers["x-forwarded-for"])
+        ? req.headers["x-forwarded-for"][0]
+        : req.headers["x-forwarded-for"]
+      : req.socket.remoteAddress;
+
+    if (!ip) {
+      res.status(403).send(blockIpMsg);
+      return;
+    }
+
+    const clientIP = ip.includes(":") ? ip.split(":").pop() : ip;
+
+    if (clientIP && config.ALLOWED_IPS.includes(clientIP)) {
+      next();
+    } else {
+      res.status(403).send(blockIpMsg);
+    }
   }
 };
 
