a615f53e1216ef4106450f9bf6d6e6a57629bed9
MRB <greymon647@gmail.com>
2024-11-11 09:27:59 +0330

    mrb: cu

--- src/routes/misc/apps-routes.ts
+++ src/routes/misc/apps-routes.ts
@@ -2,34 +2,6 @@ import { Express, Router } from "express";
 import fs from "fs";
 import path from "path";
 
-// export const loadRoutes = async (app: Express): Promise<void> => {
-//     const routesDir = path.join(process.cwd(), '..', 'apps');
-
-//     fs.readdir(routesDir, async (err, folders) => {
-//         if (err) {
-//             console.error('Error reading routes directory:', err);
-//             return;
-//         }
-
-//         for (const folder of folders) {
-//             const folderPath = path.join(routesDir, folder);
-//             if (fs.statSync(folderPath).isDirectory()) {
-//                 const usersRoutePath = path.join(folderPath, 'index.js');
-//                 // const productsRoutePath = path.join(folderPath, 'products.js');
-
-//                 if (fs.existsSync(usersRoutePath)) {
-//                     const { default: router }: { default: Router } = await import(usersRoutePath);
-//                     app.use(`/api/${folder}`, router);
-//                 }
-//                 // if (fs.existsSync(productsRoutePath)) {
-//                 //     const { default: router }: { default: Router } = await import(productsRoutePath);
-//                 //     app.use(`/api/${folder}/products`, router);
-//                 // }
-//             }
-//         }
-//     });
-// };
-
 export const loadRoutes = async (app: Express): Promise<void> => {
   const routesDir = path.join(process.cwd(), "..", "apps");
 
