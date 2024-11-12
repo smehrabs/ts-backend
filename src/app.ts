df64bb461d9cfb8eca66895c4ffb6e5df71737b4
MRB <greymon647@gmail.com>
2024-11-12 10:29:52 +0330

    mrb: cu

--- src/app.ts
+++ src/app.ts
@@ -10,16 +10,12 @@ import { helmetConfig } from "#config/helment";
 export function expressApp() {
   // app (express)
   const app = express();
-
   app.disable("x-powered-by"); // ?
-
   // ipv6Blocker(app); // IPv6 Blocker
-
   app.use(helmetConfig()); // helment helper
   app.use(express.json()); // json
-
   app.use(express.urlencoded({ extended: true })); // options
-  // app.use("/", loadAllRouter); // book router
+
   loadAllRouter(app).then(function () {
     app.listen(config.PORT, () => {
       console.log("Server connected, port: " + config.PORT);
