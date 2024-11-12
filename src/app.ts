9d720321c930ee30fabff3054c54f494477f4273
MRB <greymon647@gmail.com>
2024-11-12 11:52:42 +0330

    mrb: fix winston for log everthing

--- src/app.ts
+++ src/app.ts
@@ -9,9 +9,9 @@ import fs from 'fs';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = path.dirname(__filename); // این برای بدست اوردن مسیر فولدر های دیگه مثل بیلد یا دیست هست
 
-const logDir = path.join(__dirname, 'log');
+const logDir = path.join('log'); // or __dirname
 if (!fs.existsSync(logDir)) {
     fs.mkdirSync(logDir);
 }
@@ -31,15 +31,37 @@ const logger = winston.createLogger({
 export function expressApp() {
   // app (express)
   const app = express();
+  app.use(express.json());
   app.use((req, res, next) => {
-    logger.info(`Request: ${req.method} ${req.url}`);
-    next();
+      const startTime = Date.now();
+  
+      logger.info({
+          method: req.method,
+          url: req.url,
+          headers: req.headers,
+          body: req.body,
+          query: req.query,
+          params: req.params,
+          ip: req.ip,
+          timestamp: new Date().toISOString(),
+      });
+  
+      res.on('finish', () => {
+          const duration = Date.now() - startTime;
+          logger.info({
+              message: 'Response sent',
+              statusCode: res.statusCode,
+              duration: `${duration}ms`,
+              timestamp: new Date().toISOString(),
+          });
+      });
+  
+      next();
   });
 
   app.disable("x-powered-by");
   // ipv6Blocker(app); // IPv6 Blocker
   app.use(helmetConfig()); // helment helper
-  app.use(express.json()); // json
   app.use(express.urlencoded({ extended: true })); // options
 
   loadAllRouter(app).then(function () {
