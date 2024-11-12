d08a5b8df6abfbd21228a6f1e4f7120ef67ef6b4
MRB <greymon647@gmail.com>
2024-11-12 10:55:04 +0330

    mrb: add catch for init

--- src/init/index.ts
+++ src/init/index.ts
@@ -3,5 +3,10 @@ import "#utils/global/handler";
 
 // Init function, on the top
 export default async function init(): Promise<void> {
-  await ConnectDatabase(); // Check databases connected
+  try {
+    await ConnectDatabase(); // Check databases connected
+  } catch (error) {
+    console.error("init file error:", error);
+    process.exit(1);
+  }
 }
