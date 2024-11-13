7001901ef39d562bbf39bf38325d22a1189f93ff
MRB <greymon647@gmail.com>
2024-11-13 10:27:26 +0330

    mrb: add maxRetries for cluster

--- src/index.ts
+++ src/index.ts
@@ -8,22 +8,20 @@ import cluster from "cluster";
 import os from "os";
 
 const numCPUs = os.cpus().length;
+const maxRetries = 5; // Maximum number of retries for restarting a worker
 
-const startWorker = () => {
+const startWorker = (retries = 0) => {
   const worker = cluster.fork();
 
   worker.on("exit", (code, signal) => {
-    log.info(
-      `Worker ${worker.process.pid} died (code: ${code}, signal: ${signal}).`,
-    );
+    log.info(`Worker ${worker.process.pid} died (code: ${code}, signal: ${signal}).`);
 
-    if(code != 0){
-      log.info(
-        `Restarting...`,
-      );
-      startWorker(); // Restart the worker
+    if (code !== 0 && retries < maxRetries) {
+      log.info(`Restarting worker ${worker.process.pid}... Attempt ${retries + 1}`);
+      setTimeout(() => startWorker(retries + 1), 1000); // Delay before restarting
+    } else if (retries >= maxRetries) {
+      log.error(`Worker ${worker.process.pid} has exited with code ${code} after ${maxRetries} attempts. Not restarting.`);
     }
-    
   });
 };
 
@@ -41,7 +39,7 @@ if (cluster.isPrimary) {
       expressApp(); // Now call expressApp
     } catch (error) {
       log.error(`Error in worker ${process.pid}:`, error);
-      die();
+      die()
     }
   })();
 }
