6653163879ac972374f6c74fb705169fb5007806
MRB <greymon647@gmail.com>
2024-11-13 13:22:51 +0330

    mrb: fix mysql init

--- src/databases/mysql/use/create_database.ts
+++ src/databases/mysql/use/create_database.ts
@@ -9,39 +9,57 @@ import { con } from "../index.js";
 
 export default function (): Promise<boolean> {
   return new Promise((resolve, reject) => {
+    function errend(err: any) {
+      con.end();
+      return reject(err);
+    }
+
     con.connect(function (err: any) {
-      if (err) reject(err);
+      if (err) {
+        errend(err);
+      }
       // log.info("[database1] connected!");
       con.query(CHECK_DATABASE, function (err: any, results: any) {
-        // Check database name
         if (err) {
-          reject(err);
+          errend(err);
         } else {
           try {
             if (results.length === 0) {
               con.query(CREATE_DATABASE, function (err: any, result: any) {
                 if (err) {
-                  reject(err);
+                  errend(err);
                 } else {
                   log.info("[database1] Database created successfully.");
-                  con.query(CR_USE);
-                  con.query(CR_ALERT);
-                  con.query(CR_FLUSH);
-                  log.info(
-                    "[database1] User altered and privileges flushed successfully.",
-                  );
-                  resolve(true);
+                  con.query(CR_USE, function (err: any) {
+                    if (err) {
+                      errend(err);
+                    }
+                    con.query(CR_ALERT, function (err: any) {
+                      if (err) {
+                        errend(err);
+                      }
+                      con.query(CR_FLUSH, function (err: any) {
+                        if (err) {
+                          errend(err);
+                        }
+                        log.info(
+                          "[database1] User altered and privileges flushed successfully."
+                        );
+                        con.end();
+                        resolve(true);
+                      });
+                    });
+                  });
                 }
               });
             } else {
               // log.info('[database1] Database already exists. Skipping user alteration.');
+              con.end();
               resolve(true);
             }
           } catch (error) {
             log.error("Error executing queries:", error);
-            resolve(false);
-          } finally {
-            con.end();
+            errend(err)
           }
         }
       });
