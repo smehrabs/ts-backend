c4140489a22eb00e0cd111fc1f3797c3af57cc05
MRB <greymon647@gmail.com>
2024-11-14 13:11:37 +0330

    mrb: fix responseSentMiddleware (finally test)

--- src/middleware/race/resSentRace.ts
+++ src/middleware/race/resSentRace.ts
@@ -1,44 +1,30 @@
 import { Request, Response, NextFunction } from "express";
-import { EventEmitter } from "events";
-
-const responseSentEmitter = new EventEmitter();
 
 export const responseSentMiddleware = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  res.locals.responseSent = false;
-
   const originalSend = res.send.bind(res);
+  const originalJson = res.json.bind(res);
 
-  const preventMultipleResponses = (originalMethod: Function) => {
-    return function (...args: any[]) {
-      if (res.locals.responseSent) {
-        log.error(
-          `Attempted to send response multiple times for request: ${req.method} ${req.url}`
-        );
-        return res;
+  res.send = function (...args: any[]): Response {
+      if (!res.headersSent) {
+          return originalSend(...args);
+      } else {
+          console.warn('Attempted to send response after headers were sent.');
+          return res;
       }
-      res.locals.responseSent = true;
-      responseSentEmitter.emit("responseSent", req); // Emit the event
-      return originalMethod(...args);
-    };
   };
 
-  res.send = preventMultipleResponses(originalSend);
-
-  // Listen for the responseSent event only once
-  responseSentEmitter.once("responseSent", (request) => {
-    log.info(`Response sent for request: ${request.method} ${request.url}`);
-  });
-
-  // Error handling
-  res.on("finish", () => {
-    if (!res.locals.responseSent) {
-      log.error(`Response not sent for request: ${req.method} ${req.url}`);
-    }
-  });
+  res.json = function (...args: any[]): Response {
+      if (!res.headersSent) {
+          return originalJson(...args);
+      } else {
+          console.warn('Attempted to send JSON response after headers were sent.');
+          return res;
+      }
+  };
 
   next();
 };
