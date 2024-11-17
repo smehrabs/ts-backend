d6f841963d9e295d0004a75920c74778664db937
MRB <greymon647@gmail.com>
2024-11-17 12:48:15 +0330

    mrb: cu

--- src/controllers/books/save.ts
+++ src/controllers/books/save.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import Joi from "joi";
 import { call } from "#modules/c-call";
+
 const bookSchema = Joi.object({
   title: Joi.string().required(),
   description: Joi.string().required(),
@@ -11,7 +12,7 @@ export const saveBookController = async (req: Request, res: Response) => {
   if (error) {
     res.status(400).json({ error: error.details[0].message });
   } else {
-    const { title, description } = req.body; // TODO() Check this params with JOI
+    const { title, description } = req.body;
     try {
       const result = await call("save", title, description);
       res.send({ success: result });
