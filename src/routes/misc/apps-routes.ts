0903064fe40163f7b37eca1eac3efa3d9a7808c9
MRB <greymon647@gmail.com>
2024-11-19 11:12:40 +0330

    mrb: add Promise & fix eslint error

--- src/routes/misc/apps-routes.ts
+++ src/routes/misc/apps-routes.ts
@@ -7,21 +7,23 @@ export const loadRoutes = async (
   app: Express,
   routes: readonly string[],
 ): Promise<void> => {
-  for (const routersName of routes) {
-    let pathRoute = `/${routersName}`;
+  await Promise.all(
+    routes.map(async (routersName) => {
+      let pathRoute = `/${routersName}`;
 
-    if (routersName === 'latest') {
-      pathRoute = '/';
-    }
+      if (routersName === 'latest') {
+        pathRoute = '/';
+      }
 
-    try {
-      await loadRouter(
-        app,
-        DEF_PATH_ROUTES + `/${routersName}/` + DEF_ROUTE_FILE,
-        pathRoute,
-      );
-    } catch (error) {
-      log.error('[route loader] This is an unrecoverable error!');
-    }
-  }
+      try {
+        await loadRouter(
+          app,
+          DEF_PATH_ROUTES + `/${routersName}/` + DEF_ROUTE_FILE,
+          pathRoute,
+        );
+      } catch (error) {
+        log.error('[route loader] This is an unrecoverable error!');
+      }
+    })
+  );
 };
