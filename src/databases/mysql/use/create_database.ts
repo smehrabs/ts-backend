5d88a8085178ee53aae9914622ef87eb83449e9c
MRB <greymon647@gmail.com>
2024-11-19 09:52:37 +0330

    mrb: spell

--- src/databases/mysql/use/create_database.ts
+++ src/databases/mysql/use/create_database.ts
@@ -9,38 +9,38 @@ import {
 
 export default function (): Promise<boolean> {
   return new Promise((resolve, reject) => {
-    function errend(err: any) {
+    function errEnd(err: any) {
       con.end();
       return reject(err);
     }
 
     con.connect(function (err: any) {
       if (err) {
-        errend(err);
+        errEnd(err);
       }
       // log.info("[database1] connected!");
       con.query(CHECK_DATABASE, function (err: any, results: any) {
         if (err) {
-          errend(err);
+          errEnd(err);
         } else {
           try {
             if (results.length === 0) {
               con.query(CREATE_DATABASE, function (err: any, _result: any) {
                 if (err) {
-                  errend(err);
+                  errEnd(err);
                 } else {
                   log.info('[database1] Database created successfully.');
                   con.query(CR_USE, function (err: any) {
                     if (err) {
-                      errend(err);
+                      errEnd(err);
                     }
                     con.query(CR_ALERT, function (err: any) {
                       if (err) {
-                        errend(err);
+                        errEnd(err);
                       }
                       con.query(CR_FLUSH, function (err: any) {
                         if (err) {
-                          errend(err);
+                          errEnd(err);
                         }
                         log.info(
                           '[database1] User altered and privileges flushed successfully.',
@@ -59,7 +59,7 @@ export default function (): Promise<boolean> {
             }
           } catch (error) {
             log.error('Error executing queries:', error);
-            errend(err);
+            errEnd(err);
           }
         }
       });
