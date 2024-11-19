f8b928856b16f81a1eff7426e53d5d072c70b75c
MRB <greymon647@gmail.com>
2024-11-19 10:18:47 +0330

    mrb: add safeCleanBool for freeAll

--- src/free/index.ts
+++ src/free/index.ts
@@ -1,11 +1,15 @@
 import { freeMongo } from './mongoose.js';
 import { freeMysql } from './mysql2.js';
 
+let safeCleanBool = 0;
+
 export async function freeAll() {
+  if(safeCleanBool === 1) return
+  safeCleanBool = 1;
   try {
     freeMysql();
     await freeMongo();
   } catch (error) {
-    die();
+    console.log("[core] ERR when free connections: " + error)
   }
 }
