91c1ebe84a336bdcb71d4e60999e326ceb6afdbd
MRB <greymon647@gmail.com>
2024-11-19 11:43:22 +0330

    mrb: fix eslint error

--- src/routes/apps/latest/middleware/race/resSentRace.ts
+++ src/routes/apps/latest/middleware/race/resSentRace.ts
@@ -8,6 +8,7 @@ export const responseSentMiddleware = (
   const originalSend = res.send.bind(res);
   const originalJson = res.json.bind(res);
 
+  // eslint-disable-next-line functional/immutable-data
   res.send = function (...args: readonly any[]): Response {
     if (!res.headersSent) {
       return originalSend(...args);
@@ -17,6 +18,7 @@ export const responseSentMiddleware = (
     }
   };
 
+  // eslint-disable-next-line functional/immutable-data
   res.json = function (...args: readonly any[]): Response {
     if (!res.headersSent) {
       return originalJson(...args);
