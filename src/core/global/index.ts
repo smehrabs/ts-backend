0b8b51e01874e7f6488e257fc6fe12e9fba239d5
MRB <greymon647@gmail.com>
2024-11-21 13:12:58 +0330

    mrb: move some utils to core

--- src/core/global/index.ts
+++ src/core/global/index.ts
@@ -1,8 +1,8 @@
 /* eslint-disable functional/immutable-data */
 import { die } from '#core/utils/die';
 import { log } from '#core/utils/log';
-import { assert } from '#utils/assert';
-import { quit } from '#utils/quit';
+import { assert } from '#core/utils/assert';
+import { quit } from '#core/utils/quit';
 
 globalThis.log = log;
 globalThis.die = die;
