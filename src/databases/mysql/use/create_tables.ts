411048df65528d4623eea0d99ffd2fc8570ca65c
MRB <greymon647@gmail.com>
2024-11-26 10:45:57 +0330

    mrb: fix warn

--- src/databases/mysql/use/create_tables.ts
+++ src/databases/mysql/use/create_tables.ts
@@ -19,13 +19,17 @@ import { config } from "#config/env_get";
 
 export const createTable = async () => {
   try {
-    await mdb.schema.createTableIfNotExists(config.tc_book_name, (table) => {
-      table.increments('id').primary();
-      table.string('title', 255).notNullable();
-      table.string('description', 255).notNullable();
-    });
-    console.log('New table created: ' + config.tc_book_name);
-
+    const exists = await mdb.schema.hasTable(config.tc_book_name);
+    if (!exists) {
+      await mdb.schema.createTable(config.tc_book_name, (table) => {
+        table.increments('id').primary();
+        table.string('title', 255).notNullable();
+        table.string('description', 255).notNullable();
+      });
+      console.log('New table created: ' + config.tc_book_name);
+    } else {
+      console.log('Table already exists: ' + config.tc_book_name);
+    }
   } catch (error) {
     console.error('Error occurred:', error);
   }
