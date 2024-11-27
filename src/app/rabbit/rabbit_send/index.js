d0f2e81f3e008d9e3d17ddb51498c9cc2cde59db
MRB <greymon647@gmail.com>
2024-11-27 17:46:12 +0330

    mrb: add rabbit to app

---
+++ src/app/rabbit/rabbit_send/index.js
@@ -0,0 +1,33 @@
+import amqp from 'amqplib';
+
+const queues = {
+  pending: 'status_pending',
+  processing: 'status_processing',
+  completed: 'status_completed',
+};
+
+export const sendMessage = async (message, retries = 3) => {
+  let connection;
+  try {
+    connection = await amqp.connect('amqp://localhost');
+    const channel = await connection.createChannel();
+
+    const queue = queues[message.status] || queues.pending; // اگر وضعیت نامشخص باشد، به صف pending ارسال می‌شود
+    await channel.assertQueue(queue, { durable: true });
+
+    channel.sendToQueue(queue, Buffer.from(JSON.stringify(message)), {
+      persistent: true,
+    });
+    console.log(" [x] Sent '%s' to queue '%s'", message, queue);
+    await channel.close();
+  } catch (err) {
+    console.warn('Error sending message:', err);
+    if (retries > 0) {
+      console.log(`Retrying... (${3 - retries + 1})`);
+      await sendMessage(message, retries - 1);
+    }
+  } finally {
+    if (connection) await connection.close();
+  }
+};
+
