ebe9e8b7fea78f36597ba42b82d15065f773a447
MRB <greymon647@gmail.com>
2024-12-01 15:43:05 +0330

    mrb: fix core & OOP 1

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,48 @@
 /**
  * @link https://github.com/S-MRB-S
+ *
  * @author MRB
+ *
  */
 
-/**
- * init global lib
- */
-await import('#lib/index');
-
-/**
- * init file for index (current file)
- */
-await import('#core/init/index');
-
-$.core();
-
-export {};
 /**
  * اضافه کردن سورس های پایدار و ورژن بندی پایدار برای هسته
  * قابلیت اضافه کردن ماژول ها به هسته با قابلیت خطا پذیری برای هر ماژول
  *
  * اضافه کردن فورکر برای هر ورژن بندی بسته به تعداد و حداکثر
  */
+
+// --------------------------------------------------------------
+// init libs, init and start core
+// --------------------------------------------------------------
+void ((): void => {
+  new (class {
+    public constructor() {
+      void this.initialize();
+    }
+
+    public async initialize(): Promise<void> {
+      console.log('Application is initializing...');
+
+      /**
+       * init global lib
+       */
+      await import('#lib/index');
+
+      await this.start();
+    }
+
+    #run(): void {
+      console.log('Running the application...');
+      $.core();
+    }
+
+    public async start(): Promise<void> {
+      /**
+       * init file for index (current file)
+       */
+      await import('#core/init/index');
+      this.#run();
+    }
+  })();
+})();
--- src/lib/packages.ts
+++ src/lib/packages.ts
@@ -1,18 +1,11 @@
 export { default as cluster } from 'cluster';
-
+export { default as express } from 'express';
 export { default as fs } from 'fs';
 export { default as https } from 'https';
-export { default as path } from 'path';
-export { default as winston } from 'winston';
-
-export { default as yargs } from 'yargs/yargs';
-
-export { default as mysql } from 'mysql2';
-
 export { default as mongoose } from 'mongoose';
-
-export { default as url } from 'url';
-
+export { default as mysql } from 'mysql2';
 export { default as os } from 'os';
-
-export { default as express } from 'express';
+export { default as path } from 'path';
+export { default as url } from 'url';
+export { default as winston } from 'winston';
+export { default as yargs } from 'yargs/yargs';
