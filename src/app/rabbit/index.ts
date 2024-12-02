10d6392dbef57c3d42a7988c9cdb4931fbd5d29f
MRB <greymon647@gmail.com>
2024-12-02 17:41:38 +0330

    mrb: cu

---
+++ src/app/rabbit/index.ts
@@ -0,0 +1,14 @@
+// import { Bot } from 'grammy';
+
+// export default function (): void {
+//   const token = '7598087160:AAH9-txmznB3ExxUlT0abZUoTu8y7z0aN2Y';
+
+//   // Create a bot object
+//   const bot = new Bot(token); // <-- place your bot token in this string
+
+//   // Register listeners to handle messages
+//   bot.on('message:text', (ctx) => ctx.reply('Echo: ' + ctx.message.text));
+
+//   // Start the bot (using long polling)
+//   void bot.start();
+// }
