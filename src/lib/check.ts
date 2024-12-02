438b2ba22970a5b460ed5782e25370db7df83ce2
MRB <greymon647@gmail.com>
2024-12-02 12:24:01 +0330

    mrb: fix echo

--- src/lib/check.ts
+++ src/lib/check.ts
@@ -7,7 +7,7 @@ export async function loadLibraries(libraries: Libraries): Promise<boolean> {
   const loadPromises = libraryKeys.map((key): any => {
     try {
       const library = libraries[key];
-      echo(`Loaded ${key}`);
+      echo(`Success: Loaded ${key}`);
       return library;
     } catch (error) {
       console.error(`Error loading ${key}:`, error);
@@ -17,7 +17,7 @@ export async function loadLibraries(libraries: Libraries): Promise<boolean> {
 
   try {
     await Promise.all(loadPromises);
-    echo('All libraries loaded successfully');
+    echo('Debug: All libraries loaded successfully');
     return true;
   } catch (error) {
     console.error(`Error loading:`, error);
--- src/utils/echo.ts
+++ src/utils/echo.ts
@@ -10,22 +10,25 @@ export class InitEcho extends ConfigLoader {
     function echo(message: string): void {
       if (config.debug) {
         let color: string = '\x1b[0m'; // Default color
+        const lowerCaseMessage = message.toLowerCase();
 
         if (
-          message.includes('err') ||
-          message.includes('error') ||
-          message.includes('throw')
+          lowerCaseMessage.includes('err') ||
+          lowerCaseMessage.includes('error') ||
+          lowerCaseMessage.includes('throw')
         ) {
           color = '\x1b[31m'; // Red for errors
-        } else if (message.includes('info')) {
+        } else if (lowerCaseMessage.includes('info')) {
           color = '\x1b[32m'; // Green for info
-        } else if (message.includes('warn')) {
+        } else if (lowerCaseMessage.includes('warn')) {
           color = '\x1b[33m'; // Yellow for warnings
-        } else if (message.includes('debug')) {
+        } else if (lowerCaseMessage.includes('debug')) {
           color = '\x1b[34m'; // Blue for debug messages
+        } else if (lowerCaseMessage.includes('success')) {
+          color = '\x1b[36m'; // Cyan for success messages
         }
 
-        console.log(`${color}${message}\x1b[0m`); // چاپ پیام با رنگ مناسب
+        console.log(`${color}${message}\x1b[0m`);
       }
     }
     globalThis.echo = echo;
