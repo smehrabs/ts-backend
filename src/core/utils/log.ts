4fd97e08aef9f59590d4c9aae3af9af1c64a31de
MRB <greymon647@gmail.com>
2024-12-02 09:30:34 +0330

    mrb: fix log & OOP 5

--- src/core/utils/log.ts
+++ src/core/utils/log.ts
@@ -3,116 +3,122 @@ import winston from 'winston';
 import { logDir } from '#core/init/requirements';
 
 export interface CustomLogger extends winston.Logger {
-  core: (message: string) => void;
+  core: (message: string | object) => void;
 }
 
-$.winston.addColors({
-  core: 'magenta',
-  error: 'red',
-  warn: 'yellow',
-  debug: 'blue',
-  info: 'green',
-});
+class Logger {
+  private logger: CustomLogger;
 
-const LogLevels = {
-  core: 0,
-  error: 1,
-  warn: 2,
-  debug: 3,
-  info: 4,
-} as const;
+  public constructor() {
+    const LogLevels = {
+      core: 0,
+      error: 1,
+      warn: 2,
+      debug: 3,
+      info: 4,
+    } as const;
 
-const createFileTransport = (
-  level: keyof typeof LogLevels,
-  filename: string
-): winston.transports.FileTransportInstance => {
-  return new $.winston.transports.File({
-    filename: $.path.join(logDir, filename),
-    level,
-    handleExceptions: true,
-    format: $.winston.format.combine(
-      $.winston.format((info) => {
-        if (info.level !== level) {
-          return false;
+    winston.addColors({
+      core: 'magenta',
+      error: 'red',
+      warn: 'yellow',
+      debug: 'blue',
+      info: 'green',
+    });
+
+    const formatTimestamp = (): string => {
+      const date = new Date();
+      return date.toISOString().replace('T', ' ').substring(0, 19);
+    };
+
+    const customFormat = winston.format.printf(
+      ({ timestamp, level, message }) => {
+        let messageString: string;
+
+        if (typeof message === 'string') {
+          messageString = message;
+        } else {
+          messageString = JSON.stringify(message);
         }
-        return info;
-      })()
-    ),
-  });
-};
 
-const customFormat = $.winston.format.printf(
-  ({ timestamp, level, message }) => {
-    let messageString: string;
+        if (messageString.length > 100) {
+          messageString = messageString.substring(0, 100) + '...';
+        }
 
-    if (typeof message === 'string') {
-      messageString = message;
-    } else {
-      messageString = JSON.stringify(message);
-    }
+        return `${timestamp} ${level}: ${messageString}`;
+      }
+    );
 
-    if (messageString.length > 100) {
-      messageString = messageString.substring(0, 100) + '...';
-    }
+    const commonFormat = winston.format.combine(
+      winston.format.timestamp({ format: formatTimestamp }),
+      winston.format.json(),
+      customFormat
+    );
 
-    return `${timestamp} ${level}: ${messageString}`;
-  }
-);
+    const createFileTransport = (
+      level: keyof typeof LogLevels,
+      filename: string
+    ) => {
+      return new winston.transports.File({
+        filename: $.path.join(logDir, filename),
+        level,
+        handleExceptions: true,
+        format: winston.format.combine(
+          winston.format((info) => {
+            if (info.level !== level) {
+              return false;
+            }
+            return info;
+          })()
+        ),
+      });
+    };
 
-const formatTimestamp = (): string => {
-  const date = new Date();
-  return date.toISOString().replace('T', ' ').substring(0, 19);
-};
-
-const commonFormat = $.winston.format.combine(
-  $.winston.format.timestamp({ format: formatTimestamp }),
-  $.winston.format.json(),
-  customFormat
-);
-
-/**
- * This is a log saver (no database) with Winston.
- */
-const logger = $.winston.createLogger({
-  levels: LogLevels,
-  level: 'info',
-  exitOnError: false,
-  // format message on file (.log file)
-  format: $.winston.format.combine(
-    $.winston.format.timestamp({ format: formatTimestamp }),
-    $.winston.format.json()
-    // no need color for saving on file
-  ),
-  transports: [
-    new $.winston.transports.Console({
-      format: $.winston.format.combine(
-        $.winston.format.colorize(),
-        commonFormat
-      ),
-    }),
-    createFileTransport('core', 'core.log'),
-    createFileTransport('error', 'error.log'),
-    new $.winston.transports.File({
-      filename: $.path.join(logDir, 'combined.log'),
+    this.logger = winston.createLogger({
+      levels: LogLevels,
       level: 'info',
-      handleExceptions: true,
-      format: $.winston.format.combine(
-        $.winston.format((info) => {
-          if (info.level === 'core' || info.level === 'error') {
-            return false;
-          }
-          return info;
-        })()
+      exitOnError: false,
+      format: winston.format.combine(
+        winston.format.timestamp({ format: formatTimestamp }),
+        winston.format.json()
       ),
-    }),
-  ],
-});
+      transports: [
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.colorize(),
+            commonFormat
+          ),
+        }),
+        createFileTransport('core', 'core.log'),
+        createFileTransport('error', 'error.log'),
+        new winston.transports.File({
+          filename: $.path.join(logDir, 'combined.log'),
+          level: 'info',
+          handleExceptions: true,
+          format: winston.format.combine(
+            winston.format((info) => {
+              if (info.level === 'core' || info.level === 'error') {
+                return false;
+              }
+              return info;
+            })()
+          ),
+        }),
+      ],
+    }) as CustomLogger;
 
-export const log: CustomLogger = logger as CustomLogger;
+    this.logger.core = (message: string | object): void => {
+      this.logger.log('core', { message });
+    };
+  }
 
-log.core = (message: string | object | any): void => {
-  log.log('core', { message });
-};
+  public getLogger(): CustomLogger {
+    return this.logger;
+  }
+}
+
+const loggerInstance = new Logger();
+export const log = loggerInstance.getLogger();
 
 // Example log messages
 // log.debug('This is a debug message');
