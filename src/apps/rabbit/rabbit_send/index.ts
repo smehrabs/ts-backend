1746e51526c911e10b5ea0b1cb3cbeb6a6951ff3
MRB <greymon647@gmail.com>
2024-12-04 16:45:11 +0330

    mrb: add delay (rabbit sender)

--- src/apps/rabbit/rabbit_send/index.ts
+++ src/apps/rabbit/rabbit_send/index.ts
@@ -3,6 +3,7 @@ import amqp from 'amqplib';
 export const sendMessage = async (
   message: object,
   queue: string,
+  delay: number = 0,
   retries = 3
 ): Promise<void> => {
   let connection;
@@ -12,16 +13,26 @@ export const sendMessage = async (
 
     await channel.assertQueue(queue, { durable: true });
 
-    channel.sendToQueue(queue, Buffer.from(JSON.stringify(message)), {
+    const delayQueue = `${queue}_delay`;
+    await channel.assertQueue(delayQueue, {
+      durable: true,
+      arguments: {
+        'x-dead-letter-exchange': '',
+        'x-dead-letter-routing-key': queue,
+        'x-message-ttl': delay,
+      },
+    });
+
+    channel.sendToQueue(delayQueue, Buffer.from(JSON.stringify(message)), {
       persistent: true,
     });
-    console.log(" [x] Sent '%s' to queue '%s'", message, queue);
+    console.log(" [x] Sent '%s' to delay queue '%s'", message, delayQueue);
     await channel.close();
   } catch (err) {
     console.warn('Error sending message:', err);
     if (retries > 0) {
       console.log(`Retrying... (${3 - retries + 1})`);
-      await sendMessage(message, queue, retries - 1);
+      await sendMessage(message, queue, delay, retries - 1);
     }
   } finally {
     if (connection) await connection.close();
