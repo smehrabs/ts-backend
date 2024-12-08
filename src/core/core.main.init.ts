ea639f7d418f5772b97740d2face801da63c88e7
MRB <greymon647@gmail.com>
2024-12-08 14:13:14 +0330

    mrb: fix names

--- src/core/core.main.init.ts
+++ src/core/core.main.init.ts
@@ -1,4 +1,4 @@
-import connectDatabase from '#databases/connect';
+import connectDatabase from '#databases/database.connect.init.module';
 
 export class InitApp {
   public async initialize(): Promise<void> {
