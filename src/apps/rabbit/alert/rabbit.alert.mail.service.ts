fae451e1c35ceff39ae337f7ca2343c2f132f7da
MRB <greymon647@gmail.com>
2024-12-09 10:59:03 +0330

    mrb: fix alerts

--- src/apps/rabbit/alert/rabbit.alert.mail.service.ts
+++ src/apps/rabbit/alert/rabbit.alert.mail.service.ts
@@ -1,28 +1,30 @@
 import nodemailer from 'nodemailer';
 
-// تنظیمات SMTP
-const transporter = nodemailer.createTransport({
-  host: 'smtp.example.com', // آدرس SMTP سرور
-  port: 587, // پورت SMTP
-  secure: false, // true برای پورت 465 و false برای پورت 587
-  auth: {
-    user: 'your-email@example.com', // ایمیل شما
-    pass: 'your-email-password', // رمز عبور ایمیل شما
-  },
-});
+export function sendMail(to: string, subject: string, text: string): void {
+  // تنظیمات SMTP
+  const transporter = nodemailer.createTransport({
+    host: $.env.config.host, // آدرس SMTP سرور
+    port: $.env.config.port, // پورت SMTP
+    secure: $.env.config.secure, // true برای پورت 465 و false برای پورت 587
+    auth: {
+      user: $.env.config.auth_user, // ایمیل شما
+      pass: $.env.config.auth_pass, // رمز عبور ایمیل شما
+    },
+  });
 
-// تنظیمات ایمیل
-const mailOptions = {
-  from: 'your-email@example.com', // فرستنده
-  to: 'recipient@example.com', // گیرنده
-  subject: 'Hello from Node.js', // موضوع ایمیل
-  text: 'This is a test email sent from Node.js using TypeScript!', // متن ایمیل
-};
+  // تنظیمات ایمیل
+  const mailOptions = {
+    from: $.env.config.auth_user, // فرستنده
+    to, // گیرنده
+    subject, // موضوع ایمیل
+    text, // متن ایمیل
+  };
 
-// ارسال ایمیل
-transporter.sendMail(mailOptions, (error, info) => {
-  if (error) {
-    return console.log('Error occurred: ' + error.message);
-  }
-  console.log('Email sent: ' + info.response);
-});
+  // ارسال ایمیل
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log('Error occurred: ' + error.message);
+    }
+    console.log('Email sent: ' + info.response);
+  });
+}
--- src/apps/rabbit/alert/rabbit.alert.sms.service.ts
+++ src/apps/rabbit/alert/rabbit.alert.sms.service.ts
@@ -1,17 +1,19 @@
-// import twilio from 'twilio';
+import twilio from 'twilio';
 
-// // اطلاعات حساب Twilio
-// const accountSid = 'your_account_sid'; // SID حساب شما
-// const authToken = 'your_auth_token'; // توکن احراز هویت شما
+export function sendSms(body: string, from: string, to: string): void {
+  // اطلاعات حساب Twilio
+  const accountSid = $.env.config.accountSid; // SID حساب شما
+  const authToken = $.env.config.authToken; // توکن احراز هویت شما
 
-// const client = twilio(accountSid, authToken);
+  const client = twilio(accountSid, authToken);
 
-// // ارسال SMS
-// client.messages
-//   .create({
-//     body: 'Hello from Node.js!', // متن پیام
-//     from: 'your_twilio_number', // شماره Twilio شما
-//     to: 'recipient_number', // شماره گیرنده
-//   })
-//   .then(message => console.log('Message sent: ' + message.sid))
-//   .catch(error => console.error('Error occurred: ' + error.message));
+  // ارسال SMS
+  client.messages
+    .create({
+      body, // متن پیام
+      from, // شماره Twilio شما
+      to, // شماره گیرنده
+    })
+    .then((message) => console.log('Message sent: ' + message.sid))
+    .catch((error) => console.error('Error occurred: ' + error.message));
+}
