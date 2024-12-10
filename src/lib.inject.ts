3672e41f0e9078dc621d7f9c9be956c6eef4ec2b
MRB <greymon647@gmail.com>
2024-12-10 13:22:30 +0330

    mrb: decorators

--- src/lib.inject.ts
+++ src/lib.inject.ts
@@ -1,2 +1,27 @@
-globalThis.Core = 'core';
-globalThis.Global = 'global';
+function first() {
+  console.log('first(): factory evaluated');
+  return function (
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+  ) {
+    console.log('first(): called');
+  };
+}
+
+function second() {
+  console.log('second(): factory evaluated');
+  return function (
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+  ) {
+    console.log('second(): called');
+  };
+}
+
+class ExampleClass {
+  @first()
+  @second()
+  public method() {}
+}
