45dad3e37f85bb3ae3b8b9fe1408886d2a29278e
MRB <greymon647@gmail.com>
2024-12-10 14:43:19 +0330

    mrb: decorators

--- src/lib.inject.ts
+++ src/lib.inject.ts
@@ -20,8 +20,28 @@ function second() {
   };
 }
 
-class ExampleClass {
+class MyClass2 {
   @first()
+  public myMethod(): void {
+    console.log('myMethod() called');
+  }
+
   @second()
-  public method() {}
+  public anotherMethod(): void {
+    console.log('anotherMethod() called');
+  }
 }
+
+class MyClass extends MyClass2 {
+  public constructor() {
+    super();
+    this.myMethod();
+    setTimeout(() => {
+      this.anotherMethod();
+    }, 2000);
+  }
+}
+
+const myClassInstance = new MyClass();
+myClassInstance.myMethod();
+myClassInstance.anotherMethod();
