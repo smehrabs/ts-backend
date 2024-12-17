17e4a07d39782f332d9d356b47d07b8bfec81361
MRB <greymon647@gmail.com>
2024-12-17 12:26:47 +0330

    mrb: rm archives

--- src/archive/databases/mongo/codes.js
+++
@@ -1,8 +0,0 @@
-/**
- * @constant {number} DUPLICATE_ITEM
- * This constant represents the MongoDB error code for duplicate key errors.
- * Used to handle scenarios where a unique constraint is violated.
- */
-const DUPLICATE_ITEM = 11000
-
-export { DUPLICATE_ITEM }
--- src/archive/databases/mysql/codes.js
+++
@@ -1,5 +0,0 @@
-// error
-
-const DUPLICATE_TABLE = 'ER_DUP_ENTRY'
-
-export { DUPLICATE_TABLE }
