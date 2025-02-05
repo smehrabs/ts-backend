a5bf89c6cf0b33e6165c4f5d5abd975c374e2bb5
MRB <greymon647@gmail.com>
2025-02-05 10:57:05 +0330

    cu

--- src/global.d.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-var */
-import { ConfigManager } from './config.js';
-
-declare global {
-  var configs: ConfigManager;
-  function cwd(resolvePath?: string, join?: string): string;
-}
-
-export {};
